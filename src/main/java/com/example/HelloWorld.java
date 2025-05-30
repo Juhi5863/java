e22cd4c698970fce41c3e4339f96dc039c1eee8b
Kumar Parakram <101052237+KPkm25@users.noreply.github.com>
2025-05-30 16:57:30 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class HelloWorld {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(HelloWorld.class, args);
     }
 
     @GetMapping("/")
